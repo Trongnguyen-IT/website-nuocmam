189f2fa79bdf8cf892c1ec81f9b2be1ff9bf082e
Trongnguyen-IT <nguyennhattrong@gmail.com>
2021-07-11 19:34:03 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img class="logo" alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Nước mắm thượng nguồn"/>
+    <!-- <img class="logo" alt="Vue logo" src="./assets/logo.png" /> -->
+    <HelloWorld msg="Nước mắm thượng nguồn" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
@@ -23,9 +23,93 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-.logo {
-  max-width: 250px;
+
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  vertical-align: baseline;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="wrapper">
+    <div class="header">
+      <!-- Just an image -->
+      <nav class="bg-light">
+        <a class href="#">
+          <img class="logo" src="../assets/logo.png" alt="nuocmamthuongnguon" />
+        </a>
+      </nav>
+    </div>
+    <div class="introduce">
+      <div class="row">
+        <div class="col-4">
+          <img class="image-introduce" alt="nuocmamthuongnguon" src="../assets/image-intro.png" />
+        </div>
+        <div class="col-6">
+          <h1 class="title color-title">Giới thiệu chung</h1>
+          <p class="text-content">
+            Nhận thấy Cá Linh là một trong những đặc sản không thể bỏ qua của miền Tây Nam Bộ vào mùa nước nổi. Thời điểm lũ về cũng là lúc thiên nhiên hào phóng gửi tặng người dân sản vật quí. Trong số đó, Cá Linh là một đặc sản được đặc biệt trông chờ nhất. Hồng ngự là một huyện biên giới của tỉnh Đồng tháp, với nguồn nhiên liệu dồi giàu của thiên nhiên. Cá Linh được người dân vùng sông nước chế biến ra nhiều món ăn hấp dẫn, trong đó có Nước mắm Cá Linh truyền thống, đây là món gia vị không thể thiếu trong buổi cơm hàng ngày của chúng ta.
+            Việc thực hiện Dự án sản xuất nước mắm Cá Linh nhằm duy trì nghề làm nước mắm truyền thống của gia đình, cũng như đáp ứng nhu cầu thị trường. Việc sản xuất từ các nguyên liệu sẳn có trong tự nhiên, không sử dụng phẩm màu hay chất bảo quản gây ảnh hưởng sức khỏe người tiêu dùng.
+            Để làm được một sản phẩm nước mắm Cá Linh Thơm ngon, an toàn bản thân đã giành nhiều thời gian để học cách làm từ ông bà, cha mẹ, từ các cô chú lớn tuổi đến các cơ sở hiện tại, nhằm tạo ra một sản phẩm ngon, an toàn phù hợp với hương vị quê hương.
+          </p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+    <div class="body">
+      <VueSlickCarousel v-bind="settings">
+        <div class="column">
+          <img class="image-product" alt="Vue logo" src="../assets/image1.jpg" />
+        </div>
+        <div class="column">
+          <img class="image-product" alt="Vue logo" src="../assets/image3.jpg" />
+        </div>
+        <div class="column">
+          <img class="image-product" alt="Vue logo" src="../assets/image2.jpg" />
+        </div>
+        <div class="column">
+          <img class="image-product" alt="Vue logo" src="../assets/image4.jpg" />
+        </div>
+      </VueSlickCarousel>
+    </div>
+    <!-- <h1>{{ msg }}</h1> -->
     <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -26,17 +66,36 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul> -->
+    </ul>-->
   </div>
 </template>
 
 <script>
+import VueSlickCarousel from "vue-slick-carousel";
+import "vue-slick-carousel/dist/vue-slick-carousel.css";
+// optional style for arrows & dots
+import "vue-slick-carousel/dist/vue-slick-carousel-theme.css";
+
 export default {
-  name: 'HelloWorld',
+  data: function() {
+    return {
+      settings: {
+        dots: true,
+        dotsClass: "slick-dots custom-dot-class",
+        edgeFriction: 0.35,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    };
+  },
+  components: { VueSlickCarousel },
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -54,5 +113,43 @@ li {
 }
 a {
   color: #42b983;
+}
+.title {
+  font-size: 50px;
+  color: #b01f24;
+  text-align: left;
+  margin-bottom: 3rem;
+}
+.introduce {
+  padding: 2rem 6rem;
+}
+.bg-light {
+  padding: 10px 0;
+  background-color: rgba(0, 0, 0, 0.6) !important;
+}
+.wrapper {
+  background-image: url("../assets/background.jpg");
+}
+.column {
+  height: 600px;
+}
+.introduce .text-content {
+  color: #000;
+  font-size: 1rem;
+  text-align: left;
+}
+.introduce .image-introduce {
+  object-fit: contain;
+  width: 100%;
+  height: auto;
+}
+.logo {
+  max-width: 100px;
+}
+.body .image-wrap .column {
+}
+.image-product {
+  width: 100%;
+  object-fit: contain;
 }
 </style>
