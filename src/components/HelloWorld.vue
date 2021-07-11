cda1bf779f201f6754b09e9a0d70f3d95ffe2847
Trongnguyen-IT <nguyennhattrong@gmail.com>
2021-07-11 19:54:35 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,12 +8,12 @@
         </a>
       </nav>
     </div>
-    <div class="introduce">
+    <div class="introduce container my-lg-4">
       <div class="row">
-        <div class="col-4">
+        <div class="col-lg-4">
           <img class="image-introduce" alt="nuocmamthuongnguon" src="../assets/image-intro.png" />
         </div>
-        <div class="col-6">
+        <div class="col-lg-6">
           <h1 class="title color-title">Giới thiệu chung</h1>
           <p class="text-content">
             Nhận thấy Cá Linh là một trong những đặc sản không thể bỏ qua của miền Tây Nam Bộ vào mùa nước nổi. Thời điểm lũ về cũng là lúc thiên nhiên hào phóng gửi tặng người dân sản vật quí. Trong số đó, Cá Linh là một đặc sản được đặc biệt trông chờ nhất. Hồng ngự là một huyện biên giới của tỉnh Đồng tháp, với nguồn nhiên liệu dồi giàu của thiên nhiên. Cá Linh được người dân vùng sông nước chế biến ra nhiều món ăn hấp dẫn, trong đó có Nước mắm Cá Linh truyền thống, đây là món gia vị không thể thiếu trong buổi cơm hàng ngày của chúng ta.
@@ -21,7 +21,6 @@
             Để làm được một sản phẩm nước mắm Cá Linh Thơm ngon, an toàn bản thân đã giành nhiều thời gian để học cách làm từ ông bà, cha mẹ, từ các cô chú lớn tuổi đến các cơ sở hiện tại, nhằm tạo ra một sản phẩm ngon, an toàn phù hợp với hương vị quê hương.
           </p>
         </div>
-        <div></div>
       </div>
     </div>
     <div class="body">
@@ -40,33 +39,13 @@
         </div>
       </VueSlickCarousel>
     </div>
-    <!-- <h1>{{ msg }}</h1> -->
-    <!-- <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>-->
+    <footer class="footer py-lg-3">
+      <div class="container">
+        <p class="title-footer">Thông tin liên hệ</p>
+        <p>Chủ cơ sở: Bé Ngoan</p>
+        <p>Số điện thoại: 098 5341178</p>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -120,12 +99,10 @@ a {
   text-align: left;
   margin-bottom: 3rem;
 }
-.introduce {
-  padding: 2rem 6rem;
-}
+
 .bg-light {
   padding: 10px 0;
-  background-color: rgba(0, 0, 0, 0.6) !important;
+  background-color: rgba(0, 0, 0, 0.6);
 }
 .wrapper {
   background-image: url("../assets/background.jpg");
@@ -146,7 +123,16 @@ a {
 .logo {
   max-width: 100px;
 }
-.body .image-wrap .column {
+.footer {
+  background-color: rgba(0, 0, 0, 0.6);
+}
+.footer .title-footer {
+  font-size: 37px;
+  text-transform: uppercase;
+}
+.footer p {
+  font-size: 25px;
+  color: #fff;
 }
 .image-product {
   width: 100%;
